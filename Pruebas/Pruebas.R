da18f3cdf82edcc40d43619325781910be648fca
AdrianTJ <31286933+AdrianTJ@users.noreply.github.com>
2022-05-09 17:34:43 -0500

    Agregando primera versión del modelo lineal herárquico generalizado.

--- Pruebas/Pruebas.R
+++ Pruebas/Pruebas.R
@@ -38,6 +38,47 @@ data_by_age_smoking <- datos2 %>%
 ggplot(data_by_age_smoking, aes(x = age, y = success_rate)) + 
   geom_point()
 
+# Creating IDs
+
+strokes_groups$ID <- seq.int(nrow(strokes_groups))
+
+strokes2 <- datos2 %>%
+  mutate(age = round(age)) |> 
+  group_by(Residence_type, hypertension, gender, heart_disease, ever_married) %>%
+  mutate(group_id = cur_group_id())
+
+################################################################################
+
+# Modeling 
+
+climb_model <- stan_glmer(
+  stroke ~ age + bmi + avg_glucose_level +  (1 | group_id), 
+  data = strokes2, family = binomial,
+  prior_intercept = normal(0, 2.5, autoscale = TRUE),
+  prior = normal(0, 2.5, autoscale = TRUE), 
+  prior_covariance = decov(reg = 1, conc = 1, shape = 1, scale = 1),
+  chains = 1, iter = 500*2, seed = 84735
+)
+
+# Confirm prior specifications
+prior_summary(climb_model)
+
+# MCMC diagnostics
+trace <- mcmc_trace(climb_model, size = 0.1)
+trace
+mcmc_dens_overlay(climb_model)
+mcmc_acf(climb_model)
+neff_ratio(climb_model)
+rhat(climb_model)
+
+#  
+
+strokes2 %>%
+  add_fitted_draws(climb_model, n = 100, re_formula = NA) %>%
+  ggplot(aes(x = age, y = stroke, color = hypertension)) +
+  geom_line(aes(y = .value, group = paste(hypertension, .draw)), 
+            alpha = 0.1) + 
+  labs(y = "probability of success")
 
 
 
